3315459dc3544afc8371fc60162b37087307b303
VladimirRybnikov <fightmasterwhite@gmail.com>
2024-08-18 18:29:10 +1000

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+done_hw =12
+spentTime =1.5
+course ='Python'
+time_for_one_task=(spentTime/done_hw)
+print('Курс:',course,',','всего задач:',done_hw,',','затрачено часов:',spentTime,',','среднее время выполнения:',time_for_one_task,'часа.')
